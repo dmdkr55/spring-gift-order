3c221a41429876176f095abdb839bbeca369955b
Jaehyeok Kim <bbabbasi0505@naver.com>
2025-08-01 18:08:32 +0900

    refactor: Test는 H2 db를 사용

--- src/test/java/gift/controller/SocialLoginControllerTest.java
+++ src/test/java/gift/controller/SocialLoginControllerTest.java
@@ -17,9 +17,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+@ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 class SocialLoginControllerTest {
 
--- src/test/java/gift/service/KakaoServiceTest.java
+++ src/test/java/gift/service/KakaoServiceTest.java
@@ -18,9 +18,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+@ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 class KakaoServiceTest {
 
